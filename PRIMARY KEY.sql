e8625fe369355ba4e4c274f0e39c25a92fb96687
salmamaulida26 <salmamaulida411@gmail.com>
2025-09-25 16:23:12 +0700

    Add files via upload

---
+++ PRIMARY KEY.sql
@@ -0,0 +1,5 @@
+--- DEFINE PRIMARY KEY
+ALTER TABLE `my-rakamin-final-project.Final_Project.Customers` ADD PRIMARY KEY (CustomerID) NOT ENFORCED;
+ALTER TABLE `my-rakamin-final-project.Final_Project.Orders` ADD PRIMARY KEY (OrderID) NOT ENFORCED;
+ALTER TABLE `my-rakamin-final-project.Final_Project.ProductCategory` ADD PRIMARY KEY (CategoryID) NOT ENFORCED;
+ALTER TABLE `my-rakamin-final-project.Final_Project.Products` ADD PRIMARY KEY (ProdNumber) NOT ENFORCED;
